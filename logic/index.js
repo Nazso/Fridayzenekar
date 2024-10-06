a07d72db0f030472267bb4a6062677dd82472edb
Nagy Zsolt <zsolt.nagy.1976@gmail.com>
2024-10-06 16:48:10 +0200

    dev logic

--- logic/index.js
+++ logic/index.js
@@ -1,12 +1,7 @@
 const menuVisibility = document.querySelector('.hamb_menu_cont');
 
 function letVisible() {
-    // document.getElementsByClassName("hamb_menu_cont").style.visibility = visible;
-    // document.getElementsByClassName("menu").style.backgroundColor = "red";
-    // document.querySelectorAll(".hamb_menu_cont")
-    // menuVisibility.innerHTML = "Hello World!"
-    // menuVisibility.style.color = "yellow"
-
+    
     // menuVisibility.style.visibility = "visible"
 
     if (menuVisibility.style.display === "none") {
